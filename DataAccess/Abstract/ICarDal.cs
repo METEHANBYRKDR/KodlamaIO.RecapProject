6584ca1c9a028f7c114be44d592e47f2c1354f9e
METEHANBYRKDR <102472877+METEHANBYRKDR@users.noreply.github.com>
2024-03-07 13:58:23 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+
+namespace DataAccess.Abstract;
+
+public interface ICarDal
+{
+    List<Car> GetAll();
+    void Add(Car car);
+    void Update(Car car);
+
+    List<Car> GetById(int id);
+    void Delete(Car car);
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1,BrandId=1,ColorId=1,DailyPrice=1000,ModelYear=1960,Description="BMW"},
+                new Car{ Id = 2,BrandId=1,ColorId=2,DailyPrice=2000,ModelYear=1920,Description="BMW"},
+                new Car{ Id = 3,BrandId=2,ColorId=3,DailyPrice=3000,ModelYear=1930,Description="AUDI"},
+                new Car{ Id = 4,BrandId=2,ColorId=3,DailyPrice=4000,ModelYear=1940,Description="AUDI"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        //public void Delete(Car car)
+        //{
+        //    Car carToDelete = _cars.SingleOrDefault(p=>p.Id == car.Id);
+        //    _cars.Remove(carToDelete);
+        //}
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(p=> p.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
